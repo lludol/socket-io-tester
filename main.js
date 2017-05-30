f18c5bff3df306f4cd95267326b860bbfb3284f4
Ludovic Lerus <lerus_l@epitech.eu>
2017-05-30 13:14:41 +0200

    refactor(main): code improved

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-const electron				= require('electron');
-const {app, BrowserWindow}	= electron;
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+const { app, BrowserWindow } = electron;
 
 let mainWindow = null;
 
+/**
+ * Create the Window of the program.
+ */
 function createWindow() {
 	mainWindow = new BrowserWindow({
-		'minWidth':		800,
-		'minHeight':	500,
-		title:			'Socket.io tester'
+		minWidth:  800,
+		minHeight: 500,
+		title:     'Socket.io tester',
 	});
 
-	mainWindow.loadURL('file://' + __dirname + '/index.html');
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes:  true,
+	}));
 
 	mainWindow.on('closed', () => {
 		mainWindow = null;
 	});
 }
 
+app.on('ready', createWindow);
+
 app.on('window-all-closed', () => {
-	if (process.platform != 'darwin') {
+	if (process.platform !== 'darwin') {
 		app.quit();
 	}
 });
-
-app.on('ready', createWindow);
 
 app.on('activate', () => {
 	if (mainWindow === null) {
